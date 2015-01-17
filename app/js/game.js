e78a7c8996a9ac503e18e2c903a2e24b5706d088
Dietrich Epp <depp@zdome.net>
2015-01-17 15:19:51 -0800

    Add island creation

--- app/js/game.js
+++ app/js/game.js
@@ -1,13 +1,79 @@
 // List of all directions.
-var DIRECTIONS = ['north', 'south', 'east', 'west'];
+var DIRECTIONS = ['up', 'down', 'right', 'left'];
+
+var OPPOSITE_DIRECTION = {
+    'up': 'down',
+    'down': 'up',
+    'right': 'left',
+    'left': 'right'
+}
 
 // List of all orders that can be given to islands.
-var ORDERS = ['move_north', 'move_south', 'move_east', 'move_west',
+var ORDERS = ['move_up', 'move_down', 'move_right', 'move_left',
               'build_building', 'build_unit'];
 
-// Island class
-function Island(x, y) {
-    // (x, y) location on the map
+// Convert (x, y) coordinates to a string.
+function point(x, y) {
+    return '' + x + ',' + y;
+}
+
+// The game state.
+function Game() {
+    // List of all islands in the game.
+    this.islands = [];
+    // List of all players in the game.
+    this.players = [];
+}
+
+// Create a grid of islands.
+Game.prototype.createIslands = function(width, height) {
+    var x, y;
+    var islands = [];
+    var locs = {};
+    if (width < 2) {
+        width = 2;
+    }
+    if (height < 2) {
+        height = 2;
+    }
+    for (y = 0; y < height; y++) {
+        for (x = 0; x < width; x++) {
+            var island = new Island(
+                islands.length,
+                -1 + 2 * x / (width - 1),
+                -1 + 2 * y / (height - 1));
+            locs[point(x, y)] = island;
+            islands.push(island);
+        }
+    }
+    for (y = 0; y < height; y++) {
+        for (x = 0; x < width; x++) {
+            var island = locs[point(x, y)];
+            if (y < height - 1) {
+                island.connect(locs[point(x,y+1)], 'up');
+            }
+            if (x < width - 1) {
+                island.connect(locs[point(x+1,y)], 'right');
+            }
+        }
+    }
+    this.islands = islands;
+}
+
+// Player class.
+function Player() {
+    // Island that the player's cursor is pointing at.
+    this.cursor = null;
+    // Number of rocket parts that the player has (0..100).
+    // Reaching 100 wins the game.
+    this.rocketParts = 0;
+}
+
+// Island class.
+function Island(index, x, y) {
+    // Unique identifier for the island.
+    this.index = index;
+    // (x, y) visual location on the map, in the range -1..+1.
     this.location = [x, y];
     // The island's owner (0..N), or -1 if nobody owns it.
     this.owner = -1;
@@ -18,7 +84,7 @@ function Island(x, y) {
     // The current orders for this island.
     this.orders = null;
     // The island's neighboring islands.
-    this.neighbors = {north:null, south:null, east:null, west:null};
+    this.neighbors = {up:null, down:null, right:null, left:null};
     // The current defensive health of this island (0..100).
     // When this reaches 0, a unit or building on the island is destroyed,
     // and defense resets to 100.
@@ -26,6 +92,16 @@ function Island(x, y) {
     // How close the island is to sinking (0..100).  When this reaches 0, the
     // island sinks into the void.
     this.sinkCounter = 100;
+}
+
+Island.prototype.connect = function(other, dir) {
+    var dir2 = OPPOSITE_DIRECTION[dir];
+    if (this.neighbors[dir] !== null || other.neighbors[dir] !== null) {
+        console.log("Cannot connect islands.");
+        return;
+    }
+    this.neighbors[dir] = other;
+    other.neighbors[dir2] = this;
 }
 
 Island.prototype.update = function() {
