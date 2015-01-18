84658c5e165b47fb287b3aa691d43958bc8be15e
Dietrich Epp <depp@zdome.net>
2015-01-18 11:21:31 -0800

    Show all player cursors

--- app/js/canvas.js
+++ app/js/canvas.js
@@ -2,6 +2,28 @@
 
 // Canvas graphics implementation
 
+var COLORS = {
+	menu_bkg: '#000000',
+	menu_item: '#AAAAAA',
+	menu_sel: '#FFFFAA',
+
+	water: '#09374F',
+	island: '#E3CC86',
+
+	p0: {
+		sel: '#FF2600'
+	},
+	p1: {
+		sel: '#527DFF'
+	},
+	p2: {
+		sel: '#FFC900'
+	},
+	p3: {
+		sel: '#00A115'
+	}
+};
+
 function CanvasUI() {
     var width = Math.max(window.innerWidth, 512);
     var height = Math.round(width * 9 / 16);
@@ -18,17 +40,16 @@ function CanvasUI() {
 CanvasUI.prototype.drawMenu = function(time, menu) {
     var cxt = this.cxt;
 
-    cxt.fillStyle = 'rgb(0, 0, 0)';
+    cxt.fillStyle = COLORS.menu_bkg;
     cxt.fillRect(0, 0, this.width, this.height);
 
     cxt.font = '48px Sans-Serif';
-    cxt.fillStyle = 'rgb(200, 200, 200)';
     for (var i = 0; i < menu.items.length; i++) {
         if (i == menu.selection) {
-            cxt.fillStyle = '#ffffaa';
+            cxt.fillStyle = COLORS.menu_sel;
             cxt.fillText('>', 20, 60 + 60 * i);
         } else {
-            cxt.fillStyle = '#aaaaaa';
+            cxt.fillStyle = COLORS.menu_item;
         }
         cxt.fillText(menu.items[i].text, 60, 60 + 60*i);
     }
@@ -40,7 +61,7 @@ CanvasUI.prototype.drawGame = function(time, game) {
     var width = this.width, height = this.height;
     var sz = width * 0.05;
 
-    cxt.fillStyle = '#869DE3';
+    cxt.fillStyle = COLORS.water;
     cxt.fillRect(0, 0, this.width, this.height);
 
     function islandLoc(island) {
@@ -49,12 +70,21 @@ CanvasUI.prototype.drawGame = function(time, game) {
                 (-loc[1] * 0.75 + 1) * (height / 2)];
     }
 
+	var clocs = {};
     for (i = 0; i < game.players.length; i++) {
         var player = game.players[i];
-        var loc = islandLoc(player.cursor.island);
-        cxt.strokeStyle = '#ff00000'
+		var island = player.cursor.island;
+        var loc = islandLoc(island);
+		var cnum = clocs[island.index];
+		if (cnum === undefined) {
+			cnum = 0;
+		}
+		clocs[island.index] = cnum + 1;
+        cxt.strokeStyle = COLORS['p' + i].sel;
+		cxt.lineWidth = 3.0;
         cxt.beginPath();
-        cxt.arc(loc[0], loc[1], sz * 1.1, 0, Math.PI*2, true);
+        cxt.arc(loc[0], loc[1], sz * (1.1 + 0.1 * cnum),
+				0, Math.PI*2, true);
         cxt.stroke();
     }
 
